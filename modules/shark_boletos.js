d3a29ba5439f6b946d61669955629f8e076b9cca
leonardo jaimes <leo777jaimes@gmail.com>
2017-08-28 21:32:51 -0500

    1

--- modules/shark_boletos.js
+++ modules/shark_boletos.js
@@ -37,10 +37,10 @@ module.exports = function () {
                     //"item_url": boletos[i].url,
                     "default_action": {
                         "type": "web_url",
-                        "url": boletos[i].url,
+                        "url": boletos[i].url//,
                         // "messenger_extensions": true,
                        // "webview_height_ratio": "tall",
-                        "fallback_url": boletos[i].url
+                       // "fallback_url": boletos[i].url
                     },
                     "buttons": [
                         {
