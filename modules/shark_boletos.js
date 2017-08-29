6a76d6a07fcc7d49e83835cbbc989d6c43efd60d
leonardo jaimes <leo777jaimes@gmail.com>
2017-08-28 19:15:47 -0500

    1

--- modules/shark_boletos.js
+++ modules/shark_boletos.js
@@ -1,27 +1,25 @@
-module.exports = function()
-{
+module.exports = function () {
 
-	return {
+    return {
 
-		start: function(senderId)
-		{
+        start: function (senderId) {
 
 
             var Message = require('../bot/messages');
             // llamamos al modulo de mensajes
             var eventResults = [];
-            Message.typingOn(senderId);      
+            Message.typingOn(senderId);
             // simulamos el tipeado
             // enviamos el mensaje    
-                  Message.sendMessage(senderId, "SHARK BOLETOS");
-                  Message.typingOff(senderId);
+            Message.sendMessage(senderId, "SHARK BOLETOS");
+            Message.typingOff(senderId);
 
-                  // tipeado off
+            // tipeado off
 
 
-                  //configuramos los boletos
+            //configuramos los boletos
             var boletos = [
-               {
+                {
                     "titulo": "Magicamp",
                     "imagen": "https://botboletos-test.herokuapp.com/images/card_magicamp.jpg",
                     "subtitulo": "Pase General + Campamento (900MX)",
@@ -29,48 +27,51 @@ module.exports = function()
 
                 }];
 
-                // creamos las tarjetas
-            for(var i = 0, c = boletos.length; i < c; i++)
-            {
+            // creamos las tarjetas
+            for (var i = 0, c = boletos.length; i < c; i++) {
 
                 eventResults.push({
                     "title": boletos[i].titulo,
                     "image_url": boletos[i].imagen,
                     "subtitle": boletos[i].subtitulo,
                     "default_action": {
-                      "type": "web_url",
-                      "url": boletos[i].url,
-                      "messenger_extensions": true,
-                      "webview_height_ratio": "tall",
-                      "fallback_url": boletos[i].url
-                    },
-                    "buttons":[
-                      {
-                        "type":"web_url",
+                        "type": "web_url",
                         "url": boletos[i].url,
-                        "title":"Reservar"
-                      }           
-                    ]      
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": boletos[i].url
+                    },
+                    "buttons": [
+                        {
+                            "type": "web_url",
+                            "url": boletos[i].url,
+                            "title": "Reservar"
+                        }
+                    ]
                 });
+
+
+
+                console.log('events Results >>>>>>>>>>>>>>>' + eventResults[i]);
             }
-     
-        
 
 
 
-            console.log('events Results >>>>>>>>>>>>>>>'+eventResults);
+
+
+            console.log('events Results >>>>>>>>>>>>>>>' + eventResults);
             // se las enviamos al cliente
             Message.genericButton(senderId, eventResults);
             // dejamos de tipear
             Message.typingOff(senderId);
             // Dentro de MAGICON.
 
-        
 
 
-		}
 
-	};
+        }
+
+    };
 
 }();
 
