164284300cfb93b7cd250d0db372aa6a3bc1d86f
leonardo jaimes <leo777jaimes@gmail.com>
2017-08-31 15:19:01 -0500

    1

--- modules/shark_boletos.js
+++ modules/shark_boletos.js
@@ -21,31 +21,38 @@ module.exports = function () {
             var boletos =
                 [
                     {
-                        "titulo": "Shark!!!!",
+                        "titulo": "Startups Showcase Early Supporter:",
+                        "imagen": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F34398973%2F221981936048%2F1%2Foriginal.jpg?w=800&rect=0%2C0%2C1680%2C840&s=9fff7c3a5c78f70de08659dd5999e766",
+                        "subtitulo": "Timely Supporter. \"you are fantastic \"  we welcome you to the monthle Live Sharks  Tank demon + pitch expo & VIP party, its a smashing success and amazing event! you will enjoy the networking, pitching, and then more networking at the VIP -after party! be sure to bring lots of bussines cards! ",
+                        "url": "https://www.eventbrite.com/register?orderid=8a613b028e8711e7b25a12a0e100dc36&client_token=3a9b3506b08345e08ec3bcd25e2abde3&eid=37138315702"
+
+                    },
+
+
+                    {
+                        "titulo": "Same day including at the door Sales",
+                        "imagen": "http://cdn.images.express.co.uk/img/dynamic/78/590x/Great-White-shark-Evans-Head-595103.jpg",
+                        "subtitulo": "Pase General + Campamento (900MX)",
+                        "url": "https://www.marinahuerta.com/magicon"
+
+                    },
+                    {
+                        "titulo": "Startups on the Stage \"Bring your A-Game!\"",
                         "imagen": "http://cdn.images.express.co.uk/img/dynamic/78/590x/Great-White-shark-Evans-Head-595103.jpg",
                         "subtitulo": "Pase General + Campamento (900MX)",
                         "url": "https://www.marinahuerta.com/magicon"
 
                     },
 
-
                     {
-                        "titulo": "Shark!!!!",
+                        "titulo": "Startups Showcase Sponsor Bronze I",
                         "imagen": "http://cdn.images.express.co.uk/img/dynamic/78/590x/Great-White-shark-Evans-Head-595103.jpg",
                         "subtitulo": "Pase General + Campamento (900MX)",
                         "url": "https://www.marinahuerta.com/magicon"
 
                     },
                     {
-                        "titulo": "Shark!!!!",
-                        "imagen": "http://cdn.images.express.co.uk/img/dynamic/78/590x/Great-White-shark-Evans-Head-595103.jpg",
-                        "subtitulo": "Pase General + Campamento (900MX)",
-                        "url": "https://www.marinahuerta.com/magicon"
-
-                    },
-
-                    {
-                        "titulo": "Shark!!!!",
+                        "titulo": "Startups Showcase Sponsor Bronze II",
                         "imagen": "http://cdn.images.express.co.uk/img/dynamic/78/590x/Great-White-shark-Evans-Head-595103.jpg",
                         "subtitulo": "Pase General + Campamento (900MX)",
                         "url": "https://www.marinahuerta.com/magicon"
