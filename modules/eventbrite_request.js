ab27f334708e7fd293cb40b38fd0f70a0db5e45b
leonardo jaimes <leo777jaimes@gmail.com>
2017-09-14 16:06:45 -0500

    1

--- modules/eventbrite_request.js
+++ modules/eventbrite_request.js
@@ -4,17 +4,51 @@ module.exports = function () {
             console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<ENTRAMOS A EVENTBRITE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><');
             var Message = require('../bot/messages');
             Message.typingOn(senderId);
-            Message.sendMessage(senderId, "Eventbrite Request !! ");
+           
             Message.typingOff(senderId);
 
 
 
+            var eventbriteAPI = require('node-eventbrite');
+            var token = 'GAPMQH6AUBENAC2SPCEX';
+            try {
+                var api = eventbriteAPI({
+                    token: token,
+                    version: 'v3'
+                });
+            } catch (error) {
+                console.log(error.message); // the options are missing, this function throws an error.
+            }
+
+            api.owned_events({ user_id: 221981936048 }, function (error, data) {
+                if (error)
+                    console.log(error.message);
+                else{
+                    console.log('<<<<<<<<<<<<<<  -ENCONTRAMOS RESPUESTA- >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+                    console.log(JSON.stringify(data)); // Do something with your data!
+
+                      //Message.sendMessage(senderId, "Eventbrite Request !! ");
+
+
+                }
 
 
 
-        }
 
-    };
+                   
+            });
 
-}();
+
+
+
+
+
+
+
+
+
+
+        }//cierre de la función Start
+    };//cierre del return
+}();//cierre de la exportación del modulo
 
