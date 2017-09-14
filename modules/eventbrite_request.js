f875657b061256bf46c7e7d69475fd0a909bb6a5
leonardo jaimes <leo777jaimes@gmail.com>
2017-09-14 15:59:29 -0500

    1

--- modules/eventbrite_request.js
+++ modules/eventbrite_request.js
@@ -2,12 +2,12 @@ module.exports = function () {
     return {
         start: function (senderId) {
             console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<ENTRAMOS A EVENTBRITE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><');
-            var eventResults = [];
+            var Message = require('../bot/messages');
             Message.typingOn(senderId);
             Message.sendMessage(senderId, "Eventbrite Request !! ");
             Message.typingOff(senderId);
 
-            
+
 
 
 
