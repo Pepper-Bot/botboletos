7b876ec96a9b4e51d5d268aa59c2b967f3f3a325
leonardo jaimes <leo777jaimes@gmail.com>
2017-09-14 19:56:52 -0500

    1

--- fbObjects/ElementTemplate.js
+++ fbObjects/ElementTemplate.js
@@ -2,7 +2,7 @@
 
 module.exports = class ElementTemplate {
     constructor(title, subtitle, item_url, image_url, buttons) {
-        this.title = alto;
+        this.title = title;
         this.subtitle = subtitle;
         this.item_url = item_url;
         this.image_url = image_url;
