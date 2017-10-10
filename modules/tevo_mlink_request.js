bced3f2d08963f115d724765be89b6738cda15d7
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-10 18:46:37 -0500

    tevo

--- modules/tevo_mlink_request.js
+++ modules/tevo_mlink_request.js
@@ -13,13 +13,17 @@ module.exports = function () {
           
             var  urlApiTevo =   'https://api.ticketevolution.com/v9/ticket_groups/'+ filtro
 
-            console.log('url api tevo' +  urlApiTevo);
+            console.log('url api tevo>>>>>>>' +  urlApiTevo);
 
             if (tevoClient) {
+
                 tevoClient.getJSON(urlApiTevo).then((json) => {
                     Message.sendMessage(senderId, "Obteniendo Eventos:");
-
-                    console.log('Got events from API.>>>>>>>>>>>>>>>>>>>>>>', json);
+                    if(json.error){
+                        Message.sendMessage(senderId, json.error);
+                    }
+                            
+           
 
 
 
