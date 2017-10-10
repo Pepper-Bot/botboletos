c6462fd6ac661cc65311346565f8b90a2fed58ba
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-10 10:31:59 -0500

    t1

--- modules/tevo_mlink_request.js
+++ modules/tevo_mlink_request.js
@@ -6,15 +6,15 @@ module.exports = function () {
         start: function (senderId) {
            var TevoClient = require('ticketevolution-node');
            var Message = require('../bot/messages');
-           /* var teClient = new TevoClient({
+            var teClient = new TevoClient({
                 apiToken: '9853014b1eff3bbf8cb205f60ab1b177',
                 apiSecretKey: 'UjFcR/nPkgiFchBYjLOMTAeDRCliwyhU8mlaQni2'
-            });*/
+            });
 
 
-            //if(teClient){
+             if(teClient){
                 Message.sendMessage(senderId, "Entramos a TEVO");
-            //}
+            }
         }
     }
 }();
