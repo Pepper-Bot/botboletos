f485882623753f035ed9423a245a7234620c2e13
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-10 19:37:04 -0500

    tevo

--- modules/tevo_mlink_request.js
+++ modules/tevo_mlink_request.js
@@ -26,6 +26,7 @@ module.exports = function () {
                     }
                     else{
                         console.log("ESTE ES EL ID DEL EVENTO>>>>>>>>>>>>> "+ json);
+                        console.log("ESTE ES EL ID DEL EVENTO>>>>>>>>>>>>> "+  inspeccionar(json));
                     }
                             
            
@@ -39,3 +40,30 @@ module.exports = function () {
         }
     }
 }();
+
+
+
+function inspeccionar(obj)
+{
+  var msg = '';
+
+  for (var property in obj)
+  {
+    if (typeof obj[property] == 'function')
+    {
+      var inicio = obj[property].toString().indexOf('function');
+      var fin = obj[property].toString().indexOf(')')+1;
+      var propertyValue=obj[property].toString().substring(inicio,fin);
+      msg +=(typeof obj[property])+' '+property+' : '+propertyValue+' ;\n';
+    }
+    else if (typeof obj[property] == 'unknown')
+    {
+      msg += 'unknown '+property+' : unknown ;\n';
+    }
+    else
+    {
+      msg +=(typeof obj[property])+' '+property+' : '+obj[property]+' ;\n';
+    }
+  }
+  return msg;
+}
