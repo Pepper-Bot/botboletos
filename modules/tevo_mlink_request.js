16ad59ffcd017a30b17f413204c1fde2b17239b2
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-11 11:16:47 -0500

    1

--- modules/tevo_mlink_request.js
+++ modules/tevo_mlink_request.js
@@ -42,7 +42,7 @@ module.exports = function () {
                                          resultEvent[0] = json;
                                          var eventButtons_ =  getTemplate(resultEvent, senderId);
                                          console.log("INSPECCIONANDO EVENT BUTTONS:::::>>>>>>>>>>>>> " +
-                                         inspeccionar(eventButtons_));
+                                         inspeccionar2(eventButtons_));
                                          var gButtons =   setImageTemplate(eventButtons_);
                                         //Message.genericButton(senderId, gButtons);
 
@@ -127,4 +127,29 @@ function inspeccionar(obj) {
         }
     }
     return msg;
+}
+
+function inspeccionar2(obj)
+{
+  var msg = new Array();
+
+  for (var property in obj)
+  {
+    if (typeof obj[property] == 'function')
+    {
+      var inicio = obj[property].toString().indexOf('function');
+      var fin = obj[property].toString().indexOf(')')+1;
+      var propertyValue=obj[property].toString().substring(inicio,fin);
+      msg[msg.length] = {'type' : (typeof obj[property]), 'name' : property, 'value' : propertyValue};
+    }
+    else if (typeof obj[property] == 'unknown')
+    {
+      msg[msg.length] = {'type' : 'unknown', 'name' : property, value : 'unknown'};
+    }
+    else
+    {
+      msg[msg.length] ={'type' : (typeof obj[property]), 'name' : property, 'value' : obj[property]};
+    }
+  }
+  return msg;
 }
