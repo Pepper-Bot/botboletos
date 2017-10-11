4cc39d92117caa4aa047c3361c8fe1061b107396
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-11 12:53:32 -0500

    subiendo cheerio

--- modules/tevo_mlink_request.js
+++ modules/tevo_mlink_request.js
@@ -1,11 +1,13 @@
 
+
+var imageCards = require('../modules/imageCards'); // Google images
 module.exports = function () {
     return {
         start: function (senderId, filtro) {
             var TevoClient = require('ticketevolution-node');
             var Message = require('../bot/messages');
-            
-            var imageCards = require('../modules/imageCards'); // Google images
+
+           
 
             var tevoClient = new TevoClient({
                 apiToken: '9853014b1eff3bbf8cb205f60ab1b177',
@@ -42,14 +44,14 @@ module.exports = function () {
                                         inspeccionar(json));
 
 
-                                         var resultEvent = [];
-                                         resultEvent[0] = json;
-                                         crateTemplates(resultEvent, senderId); 
-                                          
-                                      
-                                         
-                                          
-                                        //Message.genericButton(senderId, gButtons);
+                                    var resultEvent = [];
+                                    resultEvent[0] = json;
+                                    crateTemplates(resultEvent, senderId);
+
+
+
+
+                                    //Message.genericButton(senderId, gButtons);
 
                                 }
                             }).catch((err) => {
@@ -60,7 +62,7 @@ module.exports = function () {
 
 
 
-                    }//fin  de else json.error
+                    } //fin  de else json.error
                 }).catch((err) => {
                     console.err(err);
                 });
@@ -72,35 +74,42 @@ module.exports = function () {
     }
 }();
 
-function crateTemplates(resultEvent, senderId){
-   // 
+function crateTemplates(resultEvent, senderId) {
+    // 
     var eventButtons_ = [];
-    var baseURL =  'https://ticketdelivery.herokuapp.com/event/?event_id=';
-    for(var j = 0, c = resultEvent.length; j < c; j++)
-    {
-        
+    var baseURL = 'https://ticketdelivery.herokuapp.com/event/?event_id=';
+    for (var j = 0, c = resultEvent.length; j < c; j++) {
+        getImageURL( title);
         console.log("entramos al for");
         eventButtons_.push({
             "title": resultEvent[j].name,
             "image_url": '',
             "subtitle": resultEvent[j].performances[0].performer.name,
             "default_action": {
-              "type": "web_url",
-              "url":  baseURL + resultEvent[j].id+'&uid='+senderId+'&venue_id='+ resultEvent[j].venue.id+'&performer_id='+resultEvent[j].performances[0].performer.id+'&event_name='+resultEvent[j].name,
-              "messenger_extensions": true,
-              "webview_height_ratio": "tall",
-              "fallback_url": baseURL +resultEvent[j].id+'&uid='+senderId+'&venue_id='+ resultEvent[j].venue.id+'&performer_id='+resultEvent[j].performances[0].performer.id+'&event_name='+resultEvent[j].name
+                "type": "web_url",
+                "url": baseURL + resultEvent[j].id + '&uid=' + senderId + '&venue_id=' + resultEvent[j].venue.id + '&performer_id=' + resultEvent[j].performances[0].performer.id + '&event_name=' + resultEvent[j].name,
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": baseURL + resultEvent[j].id + '&uid=' + senderId + '&venue_id=' + resultEvent[j].venue.id + '&performer_id=' + resultEvent[j].performances[0].performer.id + '&event_name=' + resultEvent[j].name
             },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url": baseURL + resultEvent[j].id+'&uid='+senderId+'&venue_id='+ resultEvent[j].venue.id+'&performer_id='+resultEvent[j].performances[0].performer.id+'&event_name='+resultEvent[j].name,
-                "title":"Book"
-              }
-            ]
+            "buttons": [{
+                "type": "web_url",
+                "url": baseURL + resultEvent[j].id + '&uid=' + senderId + '&venue_id=' + resultEvent[j].venue.id + '&performer_id=' + resultEvent[j].performances[0].performer.id + '&event_name=' + resultEvent[j].name,
+                "title": "Book"
+            }]
         });
     }
+
+}
+
+function getImageURL( title) {
   
+    var image_url = '';
+    imageCards(title, z, function (err, images, index) {
+        image_url =  images[0].url;  
+
+    });
+    console.log("IMAGEEE URL>>>>>>>>>>>"+ image_url); 
 }
 
 
@@ -123,27 +132,32 @@ function inspeccionar(obj) {
     return msg;
 }
 
-function inspeccionar2(obj)
-{
-  var msg = new Array();
+function inspeccionar2(obj) {
+    var msg = new Array();
 
-  for (var property in obj)
-  {
-    if (typeof obj[property] == 'function')
-    {
-      var inicio = obj[property].toString().indexOf('function');
-      var fin = obj[property].toString().indexOf(')')+1;
-      var propertyValue=obj[property].toString().substring(inicio,fin);
-      msg[msg.length] = {'type' : (typeof obj[property]), 'name' : property, 'value' : propertyValue};
+    for (var property in obj) {
+        if (typeof obj[property] == 'function') {
+            var inicio = obj[property].toString().indexOf('function');
+            var fin = obj[property].toString().indexOf(')') + 1;
+            var propertyValue = obj[property].toString().substring(inicio, fin);
+            msg[msg.length] = {
+                'type': (typeof obj[property]),
+                'name': property,
+                'value': propertyValue
+            };
+        } else if (typeof obj[property] == 'unknown') {
+            msg[msg.length] = {
+                'type': 'unknown',
+                'name': property,
+                value: 'unknown'
+            };
+        } else {
+            msg[msg.length] = {
+                'type': (typeof obj[property]),
+                'name': property,
+                'value': obj[property]
+            };
+        }
     }
-    else if (typeof obj[property] == 'unknown')
-    {
-      msg[msg.length] = {'type' : 'unknown', 'name' : property, value : 'unknown'};
-    }
-    else
-    {
-      msg[msg.length] ={'type' : (typeof obj[property]), 'name' : property, 'value' : obj[property]};
-    }
-  }
-  return msg;
+    return msg;
 }
