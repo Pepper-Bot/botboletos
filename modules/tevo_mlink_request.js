4dea1f6d42c6dda8634e0f566f40da8a6fbedf37
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-10 19:25:33 -0500

    index add api routs

--- modules/tevo_mlink_request.js
+++ modules/tevo_mlink_request.js
@@ -21,15 +21,12 @@ module.exports = function () {
 
                 tevoClient.getJSON(urlApiTevo).then((json) => {
                    
-                    if(json.error){
-                        Message.sendMessage(senderId, json.error);
-                    }
-                    else{
-                        Message.sendMessage(senderId, "ESTE ES EL ID DEL EVENTO>>>>>>>>>>>>> "+
-                        json.ticket_groups[0].event.id);
+
+                    Message.sendMessage(senderId, "ESTE ES EL ID DEL EVENTO>>>>>>>>>>>>> "+
+                    json.ticket_groups[0].event.id);
+
+
                       
-                    }
-                            
            
 
 
