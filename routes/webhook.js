b8e595cd4fcfe6490374849736791934d5c3cd1f
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-18 13:35:44 -0500

    find event

--- routes/webhook.js
+++ routes/webhook.js
@@ -571,7 +571,7 @@ function find_my_event(senderId) {
             var greeting = "Hi " + name;
             var messagetxt = greeting + ", Pleas enter your favorite artist, sport  team or event.";
 
-             
+            Message.sendMessage(senderId, messagetxt);
 
 
         }
