f8bd4e70b0191f94d8f5a81afb349b8f2df2e64e
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-19 16:44:04 -0500

    |

--- routes/index.js
+++ routes/index.js
@@ -56,27 +56,27 @@ router.get('/calendar', function(req, res) {
 		'<script>'+
 
 
-				(function (d, s, id) {
-					var js, fjs = d.getElementsByTagName(s)[0];
-					if (d.getElementById(id)) {
-						return;
-					}
-					js = d.createElement(s);
-					js.id = id;
-					js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
-					fjs.parentNode.insertBefore(js, fjs);
-				}(document, 'script', 'Messenger'));
+				'(function (d, s, id) {'+
+					'var js, fjs = d.getElementsByTagName(s)[0];'+
+					'if (d.getElementById(id)) {'+
+						'return;'+
+					'}'+
+					'js = d.createElement(s);'+
+					'js.id = id;'+
+					'js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";'´+
+					'fjs.parentNode.insertBefore(js, fjs);'
+				'}(document,'+ '"script"'+','+ '"Messenger"'+'));'+
 			
 			
-				window.extAsyncInit = function () {
+				'window.extAsyncInit = function () {'+
 					// the Messenger Extensions JS SDK is done loading 
-					MessengerExtensions.getUserID(function success(uids) {
-						var psid = uids.psid;
+					'MessengerExtensions.getUserID(function success(uids) {'+
+						'var psid = uids.psid;'+
 			
-					}, function error(err) {
+					'}, function error(err) {'+
 			
-					});
-				};
+					'});'+
+				'};'+
 		 
 		 +'</script>'+
 	'</head>'+
