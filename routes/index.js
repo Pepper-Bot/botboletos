51c2bd0cef0a4fd36e739c32e4e0ac8f6a520960
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-19 16:22:22 -0500

    |

--- routes/index.js
+++ routes/index.js
@@ -49,6 +49,44 @@ router.get('/', function(req, res) {
 });
 
 
+router.get('/calendar', function(req, res) {
+  var html =  " <html>"+
+	"<head>"+
+		"<title>calendar</title>"+
+		"<script>"+
+			"(function (d, s, id) {"+
+				"var js, fjs = d.getElementsByTagName(s)[0];"+
+				"if (d.getElementById(id)) {"+
+					"return;"+
+				"}"+
+				"js = d.createElement(s);"+
+				"js.id = id;"
+				"js.src = '//connect.facebook.com/en_US/messenger.Extensions.js';"+
+				"fjs.parentNode.insertBefore(js, fjs);"+
+			"}(document, 'script', 'Messenger'));"+
+	
+	
+			"window.extAsyncInit = function() {"+
+			// the Messenger Extensions JS SDK is done loading 
+			"MessengerExtensions.getUserID(function success(uids) {"+
+				"var psid = uids.psid;"+
+	
+			"}, function error(err) {"+
+	
+			"});"+
+		"};"+
+	
+		
+		"</script>"+
+	"</head>"+
+	
+	"</html>"+
+	
+	res.status(200).send(html);
+
+
+});
+
 router.get("/api/saludar/:nombre?", MLinkController.saludar);
 router.get("/api/mlink/:id?", MLinkController.getMlink);
 router.get("/api/mlinks", MLinkController.getMlinks);
