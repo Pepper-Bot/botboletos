1bbf2d4c603a0a22bce3686e5b6ca2e7effe824c
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-20 12:48:23 -0500

    1

--- routes/webhook.js
+++ routes/webhook.js
@@ -219,7 +219,7 @@ function processQuickReplies(event) {
     for (var i = 0; i < monthsReplays.length; i++) {
         if (payload == moment(monthsReplays[i]).format('MMM YYYY')) {
             choosedMonth = moment(monthsReplays[i]).format('MMM YYYY')
-            Message.sendMessage(senderId, 'Mes escogido ' + monthsReplays[i]);
+            Message.sendMessage(senderId, 'Mes escogido ' + moment(monthsReplays[i]).format('MMM YYYY'));
             break;
         }
 
