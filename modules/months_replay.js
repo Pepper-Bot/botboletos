d90aba150f43a917cde4a3033121cf7a6c8b00b5
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-20 11:05:36 -0500

    1

--- modules/months_replay.js
+++ modules/months_replay.js
@@ -11,9 +11,8 @@ module.exports = function () {
             var followMonth;
             var followMonthEnd;
             for (var i = 1; i <= 2; i++) {
-
-                 followMonth = moment(currentDate).add(1, 'M');
-                 followMonthEnd = moment(followMonth).endOf('month');
+                followMonth = moment(currentDate).add(1, 'M');
+                followMonthEnd = moment(followMonth).endOf('month');
 
                 if (currentDate.date() != followMonth.date() && followMonth.isSame(followMonthEnd.format('YYYY-MM-DD'))) {
                     followMonth = followMonth.add(1, 'd');
@@ -25,48 +24,20 @@ module.exports = function () {
             }
 
 
-            var repliesArray  =[];
+            var repliesArray = [];
             for (var i = 0; i < monthsReplays.length; i++) {
-                var currMonthName = moment(monthsReplays[i]).format('MMM');
-                console.log("MESREPLAYY >>>>" + currMonthName);
-
                 repliesArray.push({
                     "content_type": "text",
                     "title": moment(monthsReplays[i]).format('MMM YYYY'),
                     "payload": "find_my_event_month"
-    
-    
                 });
-
             }
-
-          
-
-
-            
-
 
 
             Message.markSeen(senderId);
             Message.typingOn(senderId);
 
-            var replies = [{
-                    "content_type": "text",
-                    "title": "Food",
-                    "payload": "find_my_event_month"
 
-                },
-                {
-                    "content_type": "text",
-                    "title": "Drinks",
-                    "payload": "GET_LOCATION_DRINKS"
-                },
-                {
-                    "content_type": "text",
-                    "title": "Event",
-                    "payload": "GET_LOCATION_EVENTS"
-                }
-            ];
             Message.quickReply(senderId, messagetxt, repliesArray);
             Message.typingOff(senderId);
         }
--- routes/webhook.js
+++ routes/webhook.js
@@ -210,6 +210,7 @@ function processQuickReplies(event) {
     var senderId = event.sender.id;
     var payload = event.message.quick_reply.payload;
 
+    console.log("PROPIEDADES DEL QUIK REPLY   "+inspeccionar2(event.message.quick_reply));
 
 
     switch (payload) {
@@ -460,6 +461,35 @@ function processQuickReplies(event) {
 }
 
 
+function inspeccionar2(obj) {
+    var msg = new Array();
+
+    for (var property in obj) {
+        if (typeof obj[property] == 'function') {
+            var inicio = obj[property].toString().indexOf('function');
+            var fin = obj[property].toString().indexOf(')') + 1;
+            var propertyValue = obj[property].toString().substring(inicio, fin);
+            msg[msg.length] = {
+                'type': (typeof obj[property]),
+                'name': property,
+                'value': propertyValue
+            };
+        } else if (typeof obj[property] == 'unknown') {
+            msg[msg.length] = {
+                'type': 'unknown',
+                'name': property,
+                value: 'unknown'
+            };
+        } else {
+            msg[msg.length] = {
+                'type': (typeof obj[property]),
+                'name': property,
+                'value': obj[property]
+            };
+        }
+    }
+    return msg;
+}
 
 function processPostback(event) {
 
