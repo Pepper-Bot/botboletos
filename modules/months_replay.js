2a964028486129fa2ff05cf608f603cc81b04659
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-20 11:47:07 -0500

    1

--- modules/months_replay.js
+++ modules/months_replay.js
@@ -3,26 +3,8 @@ module.exports = function () {
     return {
 
         send: function (Message, senderId, messagetxt) {
-            var moment = require('moment');
+
             var follow_months = require('./follow_months')
-
-            var monthsReplays = new Array();
-            var currentDate = moment();
-            monthsReplays.push(currentDate);
-            var followMonth;
-            var followMonthEnd;
-            for (var i = 1; i <= 2; i++) {
-                followMonth = moment(currentDate).add(1, 'M');
-                followMonthEnd = moment(followMonth).endOf('month');
-
-                if (currentDate.date() != followMonth.date() && followMonth.isSame(followMonthEnd.format('YYYY-MM-DD'))) {
-                    followMonth = followMonth.add(1, 'd');
-                }
-
-                currentDate = followMonth;
-
-                monthsReplays.push(followMonth);
-            }
 
 
             var repliesArray = [];
