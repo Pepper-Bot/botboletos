e7bbac92412771ee0f8e53ccae9e67182990aa5d
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-21 13:26:00 -0500

    1

--- modules/tevo/tevo.js
+++ modules/tevo/tevo.js
@@ -1,25 +1,19 @@
-
-
 var TevoClient = require('ticketevolution-node');
 
-function tevoClient() {
-    var tevoClient = new TevoClient({
-        apiToken: '9853014b1eff3bbf8cb205f60ab1b177',
-        apiSecretKey: 'UjFcR/nPkgiFchBYjLOMTAeDRCliwyhU8mlaQni2'
-    });
-    return tevoClient;
-}
 
+var tevoClient = new TevoClient({
+    apiToken: '9853014b1eff3bbf8cb205f60ab1b177',
+    apiSecretKey: 'UjFcR/nPkgiFchBYjLOMTAeDRCliwyhU8mlaQni2'
+});
 
 
 let searchCategoriesByParentId = (parent_id) => {
     return new Promise((res, rej) => {
-        var tevoClient = tevoClient();
         var urlApiTevo = 'https://api.ticketevolution.com/v9/categories?parent_id=' + parent_id
         console.log('>>>>>>>>>>>>>>>>>url tevo' + urlApiTevo);
         if (tevoClient) {
             tevoClient.getJSON(urlApiTevo).then((json) => {
-               res(json);
+                res(json);
             });
         }
     });
