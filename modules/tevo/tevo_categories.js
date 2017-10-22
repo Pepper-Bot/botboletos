55c2b9f8fb64620e233c4f4cf2e01a1d5187ea4a
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-22 12:20:56 -0500

    1

--- modules/tevo/tevo_categories.js
+++ modules/tevo/tevo_categories.js
@@ -99,8 +99,8 @@
 
  function searchParentCategoryByName(name) {
      var parentCategories = parentCategories();
-     /*var temp = 0;
-     for (var i = 0; i < parentCategories.length; i++) {
+    var temp = 0;
+      /*for (var i = 0; i < parentCategories.length; i++) {
 
          if (parentCategories[i].Sports) {
              text = "Sports";
