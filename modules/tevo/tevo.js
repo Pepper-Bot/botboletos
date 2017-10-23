3d0d005edef5a999fc6447f268bdd9abb5f78143
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-23 17:11:56 -0500

    1

--- modules/tevo/tevo.js
+++ modules/tevo/tevo.js
@@ -81,7 +81,7 @@ function searchEventsByParentNameSecondStep(categoriesArray, eventsArray) {
                 let events = resultado.events;
 
                 for (let j = 0; j < events.length; j++) {
-                    console.log('events[j] >>>> ' + events[j].name);
+                    //console.log('events[j] >>>> ' + events[j].name);
                     eventsArray.push({
                         "id": events[j].id,
                         "name": events[j].name,
