dba7461dd9694b2510d605685d6b4e04a92c7dc4
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-24 16:52:07 -0500

    1

--- modules/tevo/tevo.js
+++ modules/tevo/tevo.js
@@ -100,11 +100,29 @@ var setImagesToEventsTemplate = (resultEvent, gButtons, counter) => {
     gButtons = resultEvent;
 
     return new Promise((resolve, reject) => {
-        
+
         for (let z = 0, k = gButtons.length; z < k; z++) {
 
             imageCards('event ' + gButtons[z].title + ' ' + gButtons[z].image_url, z, function (err, images, index) {
-               //aki
+                let imageIndex = 0;
+                if (images.length >= 10) {
+                    imageIndex = Math.round(Math.random() * 10);
+                } else {
+                    imageIndex = Math.round(Math.random() * images.length);
+                }
+
+                if (!err) {
+                    gButtons[index].image_url = images[imageIndex].url;
+                    counter++;
+                }
+
+                if (counter == gButtons.length) {
+                    resolve(gButtons);
+                } else if (counter == 10) {
+
+                    resolve(gButtons);
+                    break;
+                }
             });
         }
     });
