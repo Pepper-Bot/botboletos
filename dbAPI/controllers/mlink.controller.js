f920abd16a8b2a35f419bdbc8b2a6b7400c70c85
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-27 12:18:24 -0500

    !

--- dbAPI/controllers/mlink.controller.js
+++ dbAPI/controllers/mlink.controller.js
@@ -417,7 +417,7 @@ function searchEventByNameAndDate(req, res) {
 	});
 
 
-	urlApiTevo = 'https://api.ticketevolution.com/v9/events?q=' + event_name + '&page=1&per_page=50&only_with_available_tickets=true&occurs_at.gte=' + occurs_at_gte + '&occurs_at.lte=' + occurs_at_lte + '&order_by=events.occurs_at'
+	var urlApiTevo = 'https://api.ticketevolution.com/v9/events?q=' + event_name + '&page=1&per_page=50&only_with_available_tickets=true&occurs_at.gte=' + occurs_at_gte + '&occurs_at.lte=' + occurs_at_lte + '&order_by=events.occurs_at'
 	console.log('>>>>>>>>>>>>>>>>>url tevo' + urlApiTevo);
 	if (tevoClient) {
 		tevoClient.getJSON(urlApiTevo).then((json) => {
