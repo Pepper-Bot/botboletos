0fa418d5473ef9459b2c27135f052eeb046b2874
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-28 16:39:04 -0500

    !

--- schemas/mongodb.js
+++ schemas/mongodb.js
@@ -1,52 +1,26 @@
-var Database = (function () {
 
-	var dbObject;
-
-	function openConnection() {
-		var mongoose = require('mongoose');
-		var connectionString = process.env.MONGODB_URI;
-
-		mongoose.connect(connectionString);
-		var object = mongoose.connection;
-		return object;
-	}
-
-	return {
-		getConnection: function () {
-			if (!dbObject) {
-				dbObject = openConnection();
-			}
-			return dbObject;
+var Database = (function(){
+	
+		var dbObject;
+	
+		function openConnection(){
+			var mongoose = require('mongoose');
+			var connectionString = process.env.MONGODB_URI;
+	
+			mongoose.connect(connectionString);
+			var object = mongoose.connection;
+			return object;
 		}
-	}
-})();
-
-
-
-var getFinalFBUserSession = (senderId, User) => {
-	return new Promise((resolve, reject) => {
-		var UserData2 = require('../schemas/userinfo')
-		UserData2.findOne({
-			fbId: senderId
-		}, {}, {
-			sort: {
-				'sessionStart': -1
+	
+		return {
+			getConnection: function(){
+				if(!dbObject) {
+						dbObject = openConnection();
+				}
+				return dbObject;
 			}
-		}, function (err, userConsultado) {
-			if (!err) {
-				resolve(userConsultado);
-			} else {
-
-			}
-		});
-	});
-}
-
-
-
-module.exports = {
-	Database,
-    getFinalFBUserSession
-
-
-};
+		}
+	})();
+	
+	module.exports = Database;
+	
