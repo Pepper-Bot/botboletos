451285de929e243d9f9f2f30d5a52cf465a3bece
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-28 16:42:52 -0500

    !

--- schemas/db_queries.js
+++ schemas/db_queries.js
@@ -1,32 +1,34 @@
 var getFinalFBUserSession = (senderId, foundUser) => {
-	return new Promise((resolve, reject) => {
-		var UserData2 = require('../schemas/userinfo')
-		UserData2.findOne({
-			fbId: senderId
-		}, {}, {
-			sort: {
-				'sessionStart': -1
-			}
-		}, function (err, foundUser) {
-			if (!err) {
-                console.log(
-                    "foundUser.fbId " + foundUser.fbId + "\n" +
-                    "foundUser.firstName " + foundUser.firstName + "\n" +
-                    "foundUser.LastName " + foundUser.LastName + "\n" +
-                    "foundUser.profilePic " + foundUser.profilePic + "\n" +
-                    "foundUser.locale " + foundUser.locale + "\n" +
-                    "foundUser.timeZone " + foundUser.timeZone + "\n" +
-                    "foundUser.gender " + foundUser.gender + "\n" +
-                    "foundUser.sessionStart " + foundUser.sessionStart + "\n" +
-                    "foundUser.eventSearchSelected " + foundUser.eventSearchSelected.length + "\n"
-                );
+    return new Promise((resolve, reject) => {
+        var UserData2 = require('../schemas/userinfo')
+        UserData2.findOne({
+            fbId: senderId
+        }, {}, {
+            sort: {
+                'sessionStart': -1
+            }
+        }, function (err, foundUser) {
+            if (!err) {
+                if (foundUser) {
+                    console.log(
+                        "foundUser.fbId " + foundUser.fbId + "\n" +
+                        "foundUser.firstName " + foundUser.firstName + "\n" +
+                        "foundUser.LastName " + foundUser.LastName + "\n" +
+                        "foundUser.profilePic " + foundUser.profilePic + "\n" +
+                        "foundUser.locale " + foundUser.locale + "\n" +
+                        "foundUser.timeZone " + foundUser.timeZone + "\n" +
+                        "foundUser.gender " + foundUser.gender + "\n" +
+                        "foundUser.sessionStart " + foundUser.sessionStart + "\n" +
+                        "foundUser.eventSearchSelected " + foundUser.eventSearchSelected.length + "\n"
+                    );
 
-				resolve(foundUser);
-			} else {
+                    resolve(foundUser);
+                }
+            } else {
 
-			}
-		});
-	});
+            }
+        });
+    });
 }
 
 module.exports = {
