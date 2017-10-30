516b31be0daa354f8e1e387c7ce657801aae44be
leonardo jaimes <leo777jaimes@gmail.com>
2017-10-30 11:33:47 -0500

    !

--- schemas/userinfo.js
+++ schemas/userinfo.js
@@ -2,26 +2,52 @@ var mongoose = require('mongoose');
 
 var UserData = mongoose.Schema({
 
-	fbId: String, 
-	firstName: String, 
+	fbId: String,
+	firstName: String,
 	LastName: String,
 	profilePic: String,
 	locale: String,
 	timeZone: String,
 	gender: String,
-	sessionStart: {type: Date, default: Date.now},
-	sessionEnd: {type: Date, default: Date.now},
+	sessionStart: {
+		type: Date,
+		default: Date.now
+	},
+	sessionEnd: {
+		type: Date,
+		default: Date.now
+	},
 	optionsSelected: [],
 	modules: [],
 	urlsVisited: [],
-	location :  { type: {type:String}, coordinates: [Number]},
+	location: {
+		type: {
+			type: String
+		},
+		coordinates: [Number]
+	},
 	locationURL: String,
-	messageNumber: {type: Number, default: 0},
+	messageNumber: {
+		type: Number,
+		default: 0
+	},
 	eventSearchSelected: [],
-	showMemore : {type: Number, default: 0},
+	showMemore: {
+		index1: {
+			type: Number,
+			default: 0
+		},
+		index2: {
+			type: Number,
+			default: 0
+		},
+		index3: {
+			type: Number,
+			default: 0
+		}
+	},
 	context: []
-	
+
 });
 
 module.exports = mongoose.model('UserData', UserData);
-
