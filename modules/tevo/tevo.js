599de4464469b87dfee48868c6b067f7121515d0
leonardo jaimes <leo777jaimes@gmail.com>
2017-11-01 13:23:00 -0500

    !

--- modules/tevo/tevo.js
+++ modules/tevo/tevo.js
@@ -281,9 +281,9 @@ function searchEventsByParentNameAndLocation(categoriesArray, eventsArray, acum,
 
             });
 
-            if (acum + 1 == categoriesArray.length) {
-                resolve(eventsArray);
-            }
+            // if (acum + 1 == categoriesArray.length) {
+            //   resolve(eventsArray);
+            //}
         }
 
     });
