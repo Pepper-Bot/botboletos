f21d620080c106bc290cf101620b197eaea8f110
leonardo jaimes <leo777jaimes@gmail.com>
2017-11-08 20:12:27 -0500

    !

--- routes/handlers-views/login.js
+++ routes/handlers-views/login.js
@@ -19,7 +19,7 @@ let personas = [{
 router.get('/', (req, res) => {
     //primero nombre del archivo y el segundo la lista de parametros con la que funciona el archivo...
     res.render(
-        './layouts/tickets/login', {titulo: "Your tickets are on its way!"}
+        './layouts/tickets/login', {titulo: "Página XD "}
     );
 
 
