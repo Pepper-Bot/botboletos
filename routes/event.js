5f0165b594aa7b18ab4e8799f713b434ea3f6d8a
leonardo jaimes <leo777jaimes@gmail.com>
2017-11-09 17:24:42 -0500

    !

--- routes/event.js
+++ routes/event.js
@@ -16,82 +16,86 @@ var teClient = new TevoClient({
 router.get('/', function (req, res) {
 
 
-	var event_id = req.query.event_id;
-	var fbId = req.query.uid;
-	var venue_id = req.query.venue_id;
-	var event_name = req.query.event_name;
-	var performer_id = req.query.performer_id;
+    var event_id = req.query.event_id;
+    var fbId = req.query.uid;
+    var venue_id = req.query.venue_id;
+    var event_name = req.query.event_name;
+    var performer_id = req.query.performer_id;
 
-	/* Prueba */
+    /* Prueba */
 
-	// Si no hay session de facebook nos salimos.
-
-	if (undefined == req.query.uid) {
-		res.status(200);
-		res.send('Error trying to access');
-		res.end();
-		return;
-	}
-	/*
-	UserData2.findOne({fbId: fbId}, {}, { sort: { 'sessionStart' : -1 } }, function(err, result)
-	{	*/
-
-	// Llamos a la API de eventos y traemos un listado de fechas. only_with_available_tickets=true
-	//teClient.getJSON(process.env.API_URL+'events/?page=1&per_page=50&performer_id='+performer_id+'&venue_id='+venue_id+'&only_with_tickets=all').then((json) => {
-	teClient.getJSON(process.env.API_URL + 'events/?page=1&per_page=50&performer_id=' + performer_id + '&venue_id=' + venue_id + '&only_with_available_tickets=true').then((json) => {
-
-		var htmlHeader = '<!DOCTYPE html><html lang="en"> <head> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no"> <base href="https://ticketdelivery.herokuapp.com/"> <title>Book</title><!--Import Google Icon Font--> <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <!--Import materialize.css--> <link type="text/css" rel="stylesheet" href="https://ticketdelivery.herokuapp.com/css/materialize.min.css" media="screen,projection"/> <!--Let browser know website is optimized for mobile--> <meta name="viewport" content="width=device-width, initial-scale=1.0"/> </head> <body> <div class="container"> <div class="row left"> <h3 class="header left blue-text">{EVENT_NAME}</h3> <div class="section"> <div class="row">';
-		var boxEvent = ' <div class="col m12 m4"> <div class="card"> <div class="card-image"> <img src="{IMAGE_EVENT}"> <span style="display:none;" class="card-title">{EVENT_NAME}</span> </div> <div class="card-content"> <p>{EVENT_DESCRIPTION} </p> <div class="card-action"> <p><a class="btn blue" href="{URL_PURCHASE_TICKET}">Purchase Tickets</a></p> </div> </div> </div> </div>';
-
-		console.log(JSON.stringify(json));
-
-		if (json) {
-			if (json.events.length > 0) {
-				var box = '';
-
-				// remplazaos la informacion en la tarjeta.
-				htmlHeader = htmlHeader.replace('{NAME_PERFORMER}', json.events[0].name);
-				htmlHeader = htmlHeader.replace('{EVENT_NAME}', json.events[0].name);
-				htmlHeader = htmlHeader.replace('{VENUE_NAME}', json.events[0].venue.name + ' ' + json.events[0].venue.location);
-
-				for (var i = 0, c = json.events.length; i < c; i++) {
-					// Creamos la tarjeta
-					var occurs_at = json.events[i].occurs_at;
-					occurs_at = occurs_at.substring(0, occurs_at.length - 4);
-
-					console.log('evento:' + json.events[i].name);
-					console.log(boxEvent);
-					console.log('-------------');
-					console.log(boxEvent.replace('{EVENT_NAME}', json.events[i].name));
-					box = boxEvent.replace('{EVENT_NAME}', json.events[i].name);
-					box = box.replace('{URL_PURCHASE_TICKET}', 'https://ticketdelivery.herokuapp.com/tickets/?event_id=' + json.events[i].id + '&uid=' + fbId + '&venue_id=' + venue_id + '&performer_id=' + performer_id + '&event_name=' + json.events[i].name + '&event_date=' + json.events[i].occurs_at + '&seatsmap=' + json.events[i].configuration.seating_chart.large);
-					htmlHeader += box.replace('{EVENT_DESCRIPTION}', json.events[i].name + ' <br> <strong>' + moment(occurs_at).format('dddd') + ', ' + moment(occurs_at).format('MMMM Do YYYY, h:mm a') + '</strong><br>' + json.events[i].venue.name + ' ' + json.events[i].venue.location);
-					box = '';
-
-				}
-
-				htmlHeader += '</div> </div> </div> </div> </div> <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script> <script type="text/javascript" src="https://ticketdelivery.herokuapp.com/js/materialize.min.js"></script> </body></html>';
+    // Si no hay session de facebook nos salimos.
+    var urlApiTevo = '';
 
 
-				// Obtenemos las imagenes de la tarjeta.
-				gis(event_name, function (err, images) {
-					res.send(htmlHeader.replace(new RegExp('{IMAGE_EVENT}', 'g'), images[0].url));
-				});
-			} else {
-				res.send('<h1>No tickets available.</h1>');
-			}
-		} else {
+    urlApiTevo = 'https://api.ticketevolution.com/v9/events/?page=1&per_page=50&performer_id=' + performer_id + '&venue_id=' + venue_id + '&only_with_available_tickets=true'
 
-			res.send('<h1>No tickets available.</h1>');
+    if (undefined == req.query.uid) {
+        res.status(200);
+        res.send('Error trying to access');
+        res.end();
+        return;
+    }
+    /*
+    UserData2.findOne({fbId: fbId}, {}, { sort: { 'sessionStart' : -1 } }, function(err, result)
+    {	*/
 
-		}
+    // Llamos a la API de eventos y traemos un listado de fechas. only_with_available_tickets=true
+    //teClient.getJSON(process.env.API_URL+'events/?page=1&per_page=50&performer_id='+performer_id+'&venue_id='+venue_id+'&only_with_tickets=all').then((json) => {
+    teClient.getJSON(urlApiTevo).then((json) => {
 
-	}).catch((err) => {
-		console.log('Error al Recuperar los eventos');
-		console.log(err);
-	});
+        var htmlHeader = '<!DOCTYPE html><html lang="en"> <head> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no"> <base href="https://botboletos-test.herokuapp.com/"> <title>Book</title><!--Import Google Icon Font--> <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <!--Import materialize.css--> <link type="text/css" rel="stylesheet" href="https://botboletos-test.herokuapp.com/css/materialize.min.css" media="screen,projection"/> <!--Let browser know website is optimized for mobile--> <meta name="viewport" content="width=device-width, initial-scale=1.0"/> </head> <body> <div class="container"> <div class="row left"> <h3 class="header left blue-text">{EVENT_NAME}</h3> <div class="section"> <div class="row">';
+        var boxEvent = ' <div class="col m12 m4"> <div class="card"> <div class="card-image"> <img src="{IMAGE_EVENT}"> <span style="display:none;" class="card-title">{EVENT_NAME}</span> </div> <div class="card-content"> <p>{EVENT_DESCRIPTION} </p> <div class="card-action"> <p><a class="btn blue" href="{URL_PURCHASE_TICKET}">Purchase Tickets</a></p> </div> </div> </div> </div>';
 
-	//});
+        console.log(JSON.stringify(json));
+
+        if (json) {
+            if (json.events.length > 0) {
+                var box = '';
+
+                // remplazaos la informacion en la tarjeta.
+                htmlHeader = htmlHeader.replace('{NAME_PERFORMER}', json.events[0].name);
+                htmlHeader = htmlHeader.replace('{EVENT_NAME}', json.events[0].name);
+                htmlHeader = htmlHeader.replace('{VENUE_NAME}', json.events[0].venue.name + ' ' + json.events[0].venue.location);
+
+                for (var i = 0, c = json.events.length; i < c; i++) {
+                    // Creamos la tarjeta
+                    var occurs_at = json.events[i].occurs_at;
+                    occurs_at = occurs_at.substring(0, occurs_at.length - 4);
+
+                    console.log('evento:' + json.events[i].name);
+                    console.log(boxEvent);
+                    console.log('-------------');
+                    console.log(boxEvent.replace('{EVENT_NAME}', json.events[i].name));
+                    box = boxEvent.replace('{EVENT_NAME}', json.events[i].name);
+                    box = box.replace('{URL_PURCHASE_TICKET}', 'https://botboletos-test.herokuapp.com/tickets/?event_id=' + json.events[i].id + '&uid=' + fbId + '&venue_id=' + venue_id + '&performer_id=' + performer_id + '&event_name=' + json.events[i].name + '&event_date=' + json.events[i].occurs_at + '&seatsmap=' + json.events[i].configuration.seating_chart.large);
+                    htmlHeader += box.replace('{EVENT_DESCRIPTION}', json.events[i].name + ' <br> <strong>' + moment(occurs_at).format('dddd') + ', ' + moment(occurs_at).format('MMMM Do YYYY, h:mm a') + '</strong><br>' + json.events[i].venue.name + ' ' + json.events[i].venue.location);
+                    box = '';
+
+                }
+
+                htmlHeader += '</div> </div> </div> </div> </div> <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script> <script type="text/javascript" src="https://botboletos-test.herokuapp.com/js/materialize.min.js"></script> </body></html>';
+
+
+                // Obtenemos las imagenes de la tarjeta.
+                gis(event_name, function (err, images) {
+                    res.send(htmlHeader.replace(new RegExp('{IMAGE_EVENT}', 'g'), images[0].url));
+                });
+            } else {
+                res.send('<h1>No tickets available.</h1>');
+            }
+        } else {
+
+            res.send('<h1>No tickets available.</h1>');
+
+        }
+
+    }).catch((err) => {
+        console.log('Error al Recuperar los eventos');
+        console.log(err);
+    });
+
+    //});
 });
 
 module.exports = router;
--- routes/groups.js
+++ routes/groups.js
@@ -7,26 +7,30 @@ var moment = require('moment');
 /* GET home page. */
 
 var TevoClient = require('ticketevolution-node'); // modulo de Ticket Evolution requests
-var teClient = new TevoClient(
-{
+var teClient = new TevoClient({
 	apiToken: process.env.API_TOKEN,
 	apiSecretKey: process.env.API_SECRET_KEY
 });
 
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 
 
- 	 var event_id = req.query.event_id;
- 	 var fbId = req.query.uid;
- 	 var venue_id = req.query.venue_id;
- 	 var event_name = req.query.event_name;
- 	 var performer_id = req.query.performer_id;
- 	 var seatsmap = req.query.seatsmap;
- 	 var event_date = req.query.event_date;
+	var event_id = req.query.event_id;
+	var fbId = req.query.uid;
+	var venue_id = req.query.venue_id;
+	var event_name = req.query.event_name;
+	var performer_id = req.query.performer_id;
+	var seatsmap = req.query.seatsmap;
+	var event_date = req.query.event_date;
 
-	if(undefined == req.query.uid)
-	{
+	var urlApiTevo = '';
+
+
+	urlApiTevo = 'https://api.ticketevolution.com/v9/ticket_groups?event_id=' + event_id + '&lightweight=true&show_past=false'
+
+
+	if (undefined == req.query.uid) {
 		res.status(200);
 		res.send('Error trying to access');
 		res.end();
@@ -36,75 +40,70 @@ router.get('/', function(req, res) {
 	UserData2.findOne({fbId: fbId}, {}, { sort: { 'sessionStart' : -1 } }, function(err, result)
 	{	*/
 
-		teClient.getJSON(process.env.API_URL+'ticket_groups?event_id='+event_id+'&lightweight=true&show_past=false').then((json) => {
+	teClient.getJSON(urlApiTevo).then((json) => {
 
 
-			console.log(JSON.stringify(json));
+		console.log(JSON.stringify(json));
 
-			var headerTickets = '<!DOCTYPE html><html lang="en"> <head> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no"> <base href="https://ticketdelivery.herokuapp.com/"> <title>Select your Tickets</title> <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script><link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/> <style>body{overflow-x:hidden;}</style> <script>$(function() {var $body = $(document);$body.bind("scroll", function() {if ($body.scrollLeft() !== 0) {$body.scrollLeft(0);}});});</script> </head> <body> <div> <div class="row"> <h3 class="header center blue-text">'+event_name+ ' Tickets</h3> <img alt="Seat Map" class="responsive-img" src="'+seatsmap+'">';
+		var headerTickets = '<!DOCTYPE html><html lang="en"> <head> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no"> <base href="https://botboletos-test.herokuapp.com/"> <title>Select your Tickets</title> <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script><link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/> <style>body{overflow-x:hidden;}</style> <script>$(function() {var $body = $(document);$body.bind("scroll", function() {if ($body.scrollLeft() !== 0) {$body.scrollLeft(0);}});});</script> </head> <body> <div> <div class="row"> <h3 class="header center blue-text">' + event_name + ' Tickets</h3> <img alt="Seat Map" class="responsive-img" src="' + seatsmap + '">';
 
-			var tableOpen = ' <table id="tickets" class="display table table-condensed table-responsive"> <thead> <tr> <th>Section</th> <th>Row</th> <th>Qty</th> <th>Price</th> <th></th> </tr> </thead> <tbody>';
-
-
-			var tableClose = '</tbody></table>';
+		var tableOpen = ' <table id="tickets" class="display table table-condensed table-responsive"> <thead> <tr> <th>Section</th> <th>Row</th> <th>Qty</th> <th>Price</th> <th></th> </tr> </thead> <tbody>';
 
 
-			var footerTickets = ' </div> </div> </div>  <script type="text/javascript" src="js/materialize.min.js"></script> <script>$(document).ready(function() { $(\'select\').material_select(); $("#tickets").DataTable( { "order": [[ 3, "desc" ]] } );} );</script> </body></html>';
-			var tableTicket = '';
+		var tableClose = '</tbody></table>';
 
-            console.log(JSON.stringify(json));
-			/*if(json.total_entries == 0)
-			{
 
-				res.send(headerTickets + '<h3>Sold out</h3>' + footerTickets);
-				res.end();
+		var footerTickets = ' </div> </div> </div>  <script type="text/javascript" src="js/materialize.min.js"></script> <script>$(document).ready(function() { $(\'select\').material_select(); $("#tickets").DataTable( { "order": [[ 3, "desc" ]] } );} );</script> </body></html>';
+		var tableTicket = '';
+
+		console.log(JSON.stringify(json));
+		/*if(json.total_entries == 0)
+		{
+
+			res.send(headerTickets + '<h3>Sold out</h3>' + footerTickets);
+			res.end();
+
+		}
+		else*/
+		{
+
+			var total = 0;
+			if (json.ticket_groups.length < 350) {
+				total = json.ticket_groups.length;
+			} else {
+				total = 350;
 
 			}
-			else*/
-			{
-
-                var total = 0;
-                if(json.ticket_groups.length < 350)
-                {
-                    total = json.ticket_groups.length;
-                }
-                else
-                {
-                    total = 350;
-
-                }
-                // limitamos el numero de tickets a 30 por materialize
-				for(var i = 0, c = total; i < c;i++)
-				{
+			// limitamos el numero de tickets a 30 por materialize
+			for (var i = 0, c = total; i < c; i++) {
 				//	if(json.ticket_groups[i].format == 'Eticket' || json.ticket_groups[i].format == 'Physical')
-					{
+				{
 
-						console.log('-----------------------------------------------------');
-						console.log(json.ticket_groups[i]);
+					console.log('-----------------------------------------------------');
+					console.log(json.ticket_groups[i]);
 
-						console.log('------------------------------------------------------');
-						var split_tickets = '';
-						for(var j = 0, d = json.ticket_groups[i].splits.length; j < d; j++ )
-						{
-							console.log(json.ticket_groups[i].splits);
-							var quantity_ = (json.ticket_groups[i].splits);
-							split_tickets += '<option value="'+quantity_[j]+'">'+quantity_[j]+'</option>';
-						}
-						tableTicket += '<tr><td>'+json.ticket_groups[i].section+'</td><td>'+json.ticket_groups[i].row+'</td><td><form action="/checkout/" method="get"><input type="hidden" value="'+json.ticket_groups[i].in_hand_on+'" name="in_hand_on"><input type="hidden" value="'+json.ticket_groups[i].in_hand+'" name="in_hand"><input type="hidden" value="'+json.ticket_groups[i].eticket+'" name="eticket"><input type="hidden" value="'+event_name+'" name="event_name"><input type="hidden" value="'+event_date+'" name="event_date"><input type="hidden" value="'+fbId+'" name="uid" id="uid"><select name="userticketsquantity" id="ticketsquantity">'+split_tickets+'</select></td><td>$'+json.ticket_groups[i].wholesale_price+'</td><td><input type="hidden" value="'+event_id+'" name="event_id"><input type="hidden" value="'+json.ticket_groups[i].id+'" name="groupticket_id"><input type="hidden" value="'+venue_id+'" name="venue_id"><input type="hidden" value="'+json.ticket_groups[i].quantity+'" name="quantity"><input type="hidden" value="'+json.ticket_groups[i].instant_delivery+'" name="instant_delivery"><input type="hidden" value="'+json.ticket_groups[i].in_hand+'" name="in_hand"><input type="hidden" value="'+json.ticket_groups[i].format+'" name="format"><input type="hidden" value="'+json.ticket_groups[i].section+'" name="section"><input type="hidden" value="'+json.ticket_groups[i].row+'" name="row"><input type="hidden" value="'+json.ticket_groups[i].wholesale_price+'" name="priceticket"><input type="hidden" value="'+json.ticket_groups[i].quantity+'" name="quantity"><button type="submit" class="btn blue">BUY</button></form></td></tr>';
+					console.log('------------------------------------------------------');
+					var split_tickets = '';
+					for (var j = 0, d = json.ticket_groups[i].splits.length; j < d; j++) {
+						console.log(json.ticket_groups[i].splits);
+						var quantity_ = (json.ticket_groups[i].splits);
+						split_tickets += '<option value="' + quantity_[j] + '">' + quantity_[j] + '</option>';
 					}
+					tableTicket += '<tr><td>' + json.ticket_groups[i].section + '</td><td>' + json.ticket_groups[i].row + '</td><td><form action="/checkout/" method="get"><input type="hidden" value="' + json.ticket_groups[i].in_hand_on + '" name="in_hand_on"><input type="hidden" value="' + json.ticket_groups[i].in_hand + '" name="in_hand"><input type="hidden" value="' + json.ticket_groups[i].eticket + '" name="eticket"><input type="hidden" value="' + event_name + '" name="event_name"><input type="hidden" value="' + event_date + '" name="event_date"><input type="hidden" value="' + fbId + '" name="uid" id="uid"><select name="userticketsquantity" id="ticketsquantity">' + split_tickets + '</select></td><td>$' + json.ticket_groups[i].wholesale_price + '</td><td><input type="hidden" value="' + event_id + '" name="event_id"><input type="hidden" value="' + json.ticket_groups[i].id + '" name="groupticket_id"><input type="hidden" value="' + venue_id + '" name="venue_id"><input type="hidden" value="' + json.ticket_groups[i].quantity + '" name="quantity"><input type="hidden" value="' + json.ticket_groups[i].instant_delivery + '" name="instant_delivery"><input type="hidden" value="' + json.ticket_groups[i].in_hand + '" name="in_hand"><input type="hidden" value="' + json.ticket_groups[i].format + '" name="format"><input type="hidden" value="' + json.ticket_groups[i].section + '" name="section"><input type="hidden" value="' + json.ticket_groups[i].row + '" name="row"><input type="hidden" value="' + json.ticket_groups[i].wholesale_price + '" name="priceticket"><input type="hidden" value="' + json.ticket_groups[i].quantity + '" name="quantity"><button type="submit" class="btn blue">BUY</button></form></td></tr>';
 				}
-				res.status(200).send(headerTickets +tableOpen +tableTicket+tableClose +footerTickets);
-				res.end();
 			}
+			res.status(200).send(headerTickets + tableOpen + tableTicket + tableClose + footerTickets);
+			res.end();
+		}
 
 
 
-		}).catch((err) => {
-			console.log('Error al Recuperar los eventos');
-		    console.log(err);
-		});
+	}).catch((err) => {
+		console.log('Error al Recuperar los eventos');
+		console.log(err);
+	});
 
- 	//});
+	//});
 });
 
 module.exports = router;
