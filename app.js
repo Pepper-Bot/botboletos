ee39e4edbf4f3c61158828c9062f1f7acded5fe6
leonardo jaimes <leo777jaimes@gmail.com>
2017-11-24 22:17:16 -0500

    !!

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var sassMiddleware = require('node-sass-middleware');
 var session = require('express-session');
 var redisClient = require('redis').createClient(process.env.REDIS_URL);
 var RedisStore = require('connect-redis')(session)
-
+var Promise = global.Promise || require('promise');
+var helpers = require('./lib/helpers');
 
 var index = require('./routes/index');
 //var users = require('./routes/users');
@@ -192,5 +193,35 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+function exposeTemplates(req, res, next) {
+  // Uses the `ExpressHandlebars` instance to get the get the **precompiled**
+  // templates which will be shared with the client-side of the app.
+  hbs.getTemplates('shared/templates/', {
+      cache      : app.enabled('view cache'),
+      precompiled: true
+  }).then(function (templates) {
+      // RegExp to remove the ".handlebars" extension from the template names.
+      var extRegex = new RegExp(hbs.extname + '$');
+
+      // Creates an array of templates which are exposed via
+      // `res.locals.templates`.
+      templates = Object.keys(templates).map(function (name) {
+          return {
+              name    : name.replace(extRegex, ''),
+              template: templates[name]
+          };
+      });
+
+      // Exposes the templates during view rendering.
+      if (templates.length) {
+          res.locals.templates = templates;
+      }
+
+      setImmediate(next);
+  })
+  .catch(next);
+}
 
 module.exports = app;
