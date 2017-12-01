3136be0f9a65095b38c3f32683fb3fa99b0e1f4f
leonardo jaimes <leo777jaimes@gmail.com>
2017-11-30 19:37:24 -0500

    3##

--- routes/tickets-views/checkout.view.js
+++ routes/tickets-views/checkout.view.js
@@ -111,7 +111,7 @@ function checkout(req, res) {
 const express = require('express');
 const paypal = require('paypal-rest-sdk');
 
-
+ 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
     'client_id': P_CLIENT_ID,
