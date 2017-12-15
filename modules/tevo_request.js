fe745462cb9aa7e3e92e452b512ba27fcf2b97d7
leonardo jaimes <leo777jaimes@gmail.com>
2017-12-15 12:52:04 -0500

    ##

--- modules/tevo_request.js
+++ modules/tevo_request.js
@@ -204,9 +204,9 @@ module.exports = function () {
                                         gButtons[z].image_url = "https://ticketdelivery.herokuapp.com/images/ciudad.jpg" //"http://www.ideosyncmedia.org/index_htm_files/196.png"
                                     }
 
-                                    counter++;
+                                   
                                     console.log('counter> ' + counter + ' gButtons.length ' + gButtons.length);
-                                    if (z + 1 == gButtons.length) {
+                                    if (counter + 1    == gButtons.length) {
 
                                         console.log('gButtons.length> ' + gButtons.length);
 
@@ -231,6 +231,9 @@ module.exports = function () {
                                     }
 
 
+                                }).then(() => {
+                                    counter = counter + 1;
+                    
                                 });
 
 
