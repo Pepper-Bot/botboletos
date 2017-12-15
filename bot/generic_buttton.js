e1733b42ae59b3397c6906ab04906f7b154344f7
leonardo jaimes <leo777jaimes@gmail.com>
2017-12-15 11:47:30 -0500

    ##

--- bot/generic_buttton.js
+++ bot/generic_buttton.js
@@ -71,7 +71,7 @@ function genericButtonQuickReplay(senderId, gButtons, messageText) {
                     "type": _0x6b64[5],
                     "payload": {
                         "template_type": _0x6b64[6],
-                        "image_aspect_ratio": "square",
+                        "image_aspect_ratio": "horizontal",
                         "elements": gButtons
                     }
                 }
