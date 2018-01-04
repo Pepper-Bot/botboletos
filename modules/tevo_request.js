989ca2143ede3b13f66428a324e6c47a46a93da5
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-04 13:03:42 -0500

    ##g

--- modules/tevo_request.js
+++ modules/tevo_request.js
@@ -289,23 +289,7 @@ var getGoogleImage = (search, matriz = []) => {
             if (error) {
                 reject(error);
             } else {
-                var results1 = []
-                for (let i = 0; i < results.length; i++) {
-                    if (results[i].width / results[i].height >= 1.91 && results[i].width / results[i].height <= 2 && results[i].height > 300) {
-                        results1.push(results[i])
-                    }
-                    let index = results.length - 1 - i;
-                    if (results[index].width / results[index].height >= 1.91 && results[index].width / results[index].height <= 2 && results[index].height > 300) {
-                        results1.push(results[i])
-                    }
-                    if (results1.length == 4) {
-                        resolve(results1)
-                     
-                    }
-
-
-
-                }
+               resolve(results)
                 
 
             }
