e4929b9456168dc83d85b6ab217f1b7763dae3cd
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-03 19:59:14 -0500

    3#

--- modules/tevo/tevo_request_by_location.js
+++ modules/tevo/tevo_request_by_location.js
@@ -50,11 +50,11 @@ module.exports = function () {
 
                             // resultEvent = json.events;
 
-                            resultEvent = arraySort(json.events, ['popularity_score', 'occurs_at'], [{
+                            resultEvent = arraySort(json.events, ['popularity_score', 'occurs_at'], {
                                 reverse: true
                             }, {
                                 reverse: false
-                            }]);
+                            });
 
                             var eventButtons_ = [];
                             var callsGis = 0;
