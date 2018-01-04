40a278c0bde9f2e80055e98fe55709324d0a6f66
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-04 17:58:36 -0500

    ##

--- routes/tickets-controllers/event.controller.js
+++ routes/tickets-controllers/event.controller.js
@@ -41,15 +41,22 @@ var render_events = (req, res) => {
                 var events = json.events
                 var counter = 0;
                 setImagesToEvents(req, json.events, counter).then((events) => {
-                    res.render(
-                        './layouts/tickets/event', {
-                            titulo: "Book",
-                            APLICATION_URL_DOMAIN: APLICATION_URL_DOMAIN,
-                            events: events,
-                            uid: req.query.uid,
-                            subtitulo: events[0].name,
-                        }
-                    );
+                    var event_name = events[0].name
+                    gis(event_name, function (err, images) {
+                     
+                        res.render(
+                            './layouts/tickets/event', {
+                                titulo: "Book",
+                                APLICATION_URL_DOMAIN: APLICATION_URL_DOMAIN,
+                                events: events,
+                                uid: req.query.uid,
+                                subtitulo: events[0].name,
+                                image_url: images[0].url
+                            }
+                        );
+                    });
+
+
                 })
 
             } else {
@@ -74,27 +81,23 @@ var render_events = (req, res) => {
 
 var setImagesToEvents = (req, resultEvent, counter) => {
     var events = resultEvent;
+    return new Promise((resolve, reject) => {
 
-    let search = events[0].name
-    getGoogleImage(search, events).then((images) => {
-        return new Promise((resolve, reject) => {
-            for (let z = 0; z < events.length; z++) {
-                events[z].image_url = images[0].url;
-                var occurs_at = events[z].occurs_at
-                occurs_at = occurs_at.substring(0, occurs_at.length - 4)
-                occurs_at = moment(occurs_at).format('dddd') + ', ' + moment(occurs_at).format('MMMM Do YYYY, h:mm a')
-                // occurs_at = moment(occurs_at).format('MMM Do YYYY, h:mm a')
-                events[z].occurs_at_format = occurs_at
-                events[z].button_href = APLICATION_URL_DOMAIN + "tickets/?event_id=" + events[z].id + "&uid=" + req.query.uid
-                counter = counter + 1;
-                if (counter + 1 == events.length) {
-                    resolve(events)
-                }
+        for (let z = 0; z < events.length; z++) {
+            var occurs_at = events[z].occurs_at
+            occurs_at = occurs_at.substring(0, occurs_at.length - 4)
+            occurs_at = moment(occurs_at).format('dddd') + ', ' + moment(occurs_at).format('MMMM Do YYYY, h:mm a')
+            // occurs_at = moment(occurs_at).format('MMM Do YYYY, h:mm a')
+            events[z].occurs_at_format = occurs_at
+            events[z].button_href = APLICATION_URL_DOMAIN + "tickets/?event_id=" + events[z].id + "&uid=" + req.query.uid
+            counter = counter + 1;
+            if (z + 1 == events.length) {
+                resolve(events)
             }
-        });
-    })
+        }
 
 
+    });
 }
 
 
@@ -158,7 +161,7 @@ var getGoogleImage = (search, matriz = []) => {
                 reject(error);
             } else {
                 console.log("Imagenes gis Respuesta >>> " + results.length);
-               //  console.log("Imagenes gis Respuesta >>> " + JSON.stringify(results));
+                console.log("Imagenes gis Respuesta >>> " + JSON.stringify(results));
                 resolve(results, matriz);
                 /*for (let i = 0; i < results.length; i++) {
 
