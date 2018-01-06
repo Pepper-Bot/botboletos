1a998d8577f9b3fa0f3dfb2028af95c26a1742c9
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-06 13:25:43 -0500

    ##

--- bot/userinfo.js
+++ bot/userinfo.js
@@ -1,29 +1,28 @@
-var userInfo = function() 
-{
+var userInfo = function () {
 
 	var request = require('request');
 	return {
 
-		getInfo: function(userId, callback){
+		getInfo: function (userId, callback) {
 
-
-		request({
-				url: 'https://graph.facebook.com/v2.6/'+ userId,
-				qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+			const userFieldSet = 'id, name, about, email, accounts, link, is_verified, significant_other, relationship_status, website, picture, photos, feed';
+			request({
+				url: 'https://graph.facebook.com/v2.6/' + userId,
+				qs: {
+					access_token: process.env.PAGE_ACCESS_TOKEN,
+					fields: userFieldSet
+				},
 				method: "GET",
-			}, function(error, response, body){
-				if(error)
-				{
+			}, function (error, response, body) {
+				if (error) {
 					callback(true, null);
-				}
-				else
-				{
+				} else {
 					console.log("User Info >>> " + JSON.stringify(body));
 					callback(null, body);
 				}
 			});
 		}
-	};	
+	};
 }();
 
 module.exports = userInfo;
