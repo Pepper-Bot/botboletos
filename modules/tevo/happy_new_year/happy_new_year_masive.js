94b8f24fa1ccbe672ac2b03e62b704be1d4669e2
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-13 14:55:57 -0500

    ##

--- modules/tevo/happy_new_year/happy_new_year_masive.js
+++ modules/tevo/happy_new_year/happy_new_year_masive.js
@@ -1,50 +1,50 @@
 var UserQueries = require('../../../schemas/queries/user_queries')
 var happy_new_year = require('./happy_new_year')
 var APLICATION_URL_DOMAIN = require('../../../config/config_vars').APLICATION_URL_DOMAIN;
-var usuariosPrueba = [{
-    "_id": {
-        "fbId": "1552706148141481",
-        "lastName": "Silver",
-        "firstName": "Mike"
-    },
-    "cantidad": 9
-}, {
-    "_id": {
-        "fbId": "1185145998253428",
-        "lastName": "Russi",
-        "firstName": "Arjumand"
-    },
-    "cantidad": 7
-}, {
-    "_id": {
-        "fbId": "1392174490831537",
-        "lastName": "Martinez",
-        "firstName": "Laura"
-    },
-    "cantidad": 2
-}, {
-    "_id": {
-        "fbId": "1705877032758862",
-        "lastName": "Jaimes Estévez",
-        "firstName": "Leo"
-    },
-    "cantidad": 898
-}, {
-    "_id": {
-        "fbId": "1461377663955819",
-        "lastName": "Hernández Silveira",
-        "firstName": "Mariel"
-    },
-    "cantidad": 28
-}]
+
 
 var sendHappyNewYerToUsers = () => {
     UserQueries.getUsersGroupByFBId((error, usuarios) => {
         console.log("Users >>> " + JSON.stringify(usuarios));
     })
-
-    for (let i = 0; i < usuariosPrueba.length; i++) {
-         happy_new_year.start(usuariosPrueba[i]._id.fbId, true)
+    var usuarios = [{
+        "_id": {
+            "fbId": "1552706148141481",
+            "lastName": "Silver",
+            "firstName": "Mike"
+        },
+        "cantidad": 9
+    }, {
+        "_id": {
+            "fbId": "1185145998253428",
+            "lastName": "Russi",
+            "firstName": "Arjumand"
+        },
+        "cantidad": 7
+    }, {
+        "_id": {
+            "fbId": "1392174490831537",
+            "lastName": "Martinez",
+            "firstName": "Laura"
+        },
+        "cantidad": 2
+    }, {
+        "_id": {
+            "fbId": "1705877032758862",
+            "lastName": "Jaimes Estévez",
+            "firstName": "Leo"
+        },
+        "cantidad": 898
+    }, {
+        "_id": {
+            "fbId": "1461377663955819",
+            "lastName": "Hernández Silveira",
+            "firstName": "Mariel"
+        },
+        "cantidad": 28
+    }]
+    for (let i = 0; i < usuarios.length; i++) {
+         happy_new_year.start(usuarios[i]._id.fbId, true)
      }
 }
 
