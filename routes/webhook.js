2c759401874dcd53c17551c86bf5d63b06ffd867
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-22 14:28:36 -0500

    ##

--- routes/webhook.js
+++ routes/webhook.js
@@ -889,7 +889,7 @@ function handleApiAiResponse(sender, response) {
     } else if (responseText == '' && !isDefined(action)) {
         //api ai could not evaluate input.
         console.log('Unknown query' + response.result.resolvedQuery);
-        //sendTextMessage(sender, "I'm not sure what you want. Can you be more specific?");
+         sendTextMessage(sender, "I'm not sure what you want. Can you be more specific?");
 
 
 
