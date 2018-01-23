4f161a8dd60fe8330c89b53a54782733c22df89f
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-23 12:29:14 -0500

    ##

--- routes/webhook.js
+++ routes/webhook.js
@@ -146,6 +146,7 @@ function sendToApiAi(sender, text) {
 
     apiaiRequest.on('response', (response) => {
         if (isDefined(response.result)) {
+            console.log( 'Api.ai response messages' + JSON.stringify(response  )  ) 
             handleApiAiResponse(sender, response);
         }
     });
