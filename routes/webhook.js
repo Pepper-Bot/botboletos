47a85dd886515f82f6f55c3493f6387838963ca9
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-23 11:46:53 -0500

    ##

--- routes/webhook.js
+++ routes/webhook.js
@@ -380,6 +380,166 @@ function processMessage(senderId, textMessage) {
 
 }
 
+ 
+
+function handleMessage(message, sender) {
+    switch (message.type) {
+        case 0: //text
+            
+            Message.sendMessage(senderId, message.speech);
+
+            break;
+        case 2: //quick replies
+            let replies = [];
+            for (var b = 0; b < message.replies.length; b++) {
+                let reply = {
+                    "content_type": "text",
+                    "title": message.replies[b],
+                    "payload": message.replies[b]
+                }
+                replies.push(reply);
+            }
+            sendQuickReply(sender, message.title, replies);
+            break;
+        case 3: //image
+            sendImageMessage(sender, message.imageUrl);
+            break;
+        case 4:
+            // custom payload
+            var messageData = {
+                recipient: {
+                    id: sender
+                },
+                message: message.payload.facebook
+
+            };
+
+            callSendAPI(messageData);
+
+            break;
+    }
+}
+
+
+function sendImageMessage(recipientId, imageUrl) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "image",
+                payload: {
+                    url: imageUrl
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function sendQuickReply(recipientId, text, replies, metadata) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: text,
+            metadata: isDefined(metadata) ? metadata : '',
+            quick_replies: replies
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function handleCardMessages(messages, sender) {
+
+    let elements = [];
+    for (var m = 0; m < messages.length; m++) {
+        let message = messages[m];
+        let buttons = [];
+        for (var b = 0; b < message.buttons.length; b++) {
+            let isLink = (message.buttons[b].postback.substring(0, 4) === 'http');
+            let button;
+            if (isLink) {
+                button = {
+                    "type": "web_url",
+                    "title": message.buttons[b].text,
+                    "url": message.buttons[b].postback
+                }
+            } else {
+                button = {
+                    "type": "postback",
+                    "title": message.buttons[b].text,
+                    "payload": message.buttons[b].postback
+                }
+            }
+            buttons.push(button);
+        }
+
+
+        let element = {
+            "title": message.title,
+            "image_url": message.imageUrl,
+            "subtitle": message.subtitle,
+            "buttons": buttons
+        };
+        elements.push(element);
+    }
+    sendGenericMessage(sender, elements);
+}
+
+function sendGenericMessage(recipientId, elements) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: elements
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: PAGE_ACCESS_TOKEN
+        },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            if (messageId) {
+                console.log("Successfully sent message with id %s to recipient %s",
+                    messageId, recipientId);
+            } else {
+                console.log("Successfully called Send API for recipient %s",
+                    recipientId);
+            }
+        } else {
+            console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+        }
+    });
+}
+
+
+
 function processLocation(senderId, locationData) {
 
     UserData2.findOne({
