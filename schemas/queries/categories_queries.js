e41da50ca1f3b2e94ddb124e05ef4a8139a55843
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-24 20:21:04 -0500

    3#

--- schemas/queries/categories_queries.js
+++ schemas/queries/categories_queries.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 
 
 
-var getIdCategories = (event_type) => {
+var getIdCategories1 = (event_type) => {
     return new Promise((resolve, reject) => {
         CategoryModel
             .find({
@@ -35,6 +35,32 @@ var getIdCategories = (event_type) => {
     })
 }
 
+
+
+var getIdCategories = (event_type) => {
+    return new Promise((resolve, reject) => {
+        db.collection.find({
+            name: {
+                $regex: new RegExp(event_type, "ig")
+            }
+        }, function (err, categories) {
+            if (err) {
+                console.log('Error al consultar categorias por nombre getIdCategories ' + err)
+                reject(err)
+
+            }
+            if (!categories) {
+                console.log('Error al consultar categorias por nombre getIdCategories ' + err)
+                reject(err)
+            }
+            if (categories) {
+                resolve(categories)
+                console.log("categories query Tevo >>> " + JSON.stringify(categories));
+            }
+        });
+
+    })
+}
 
 
 module.exports = {
