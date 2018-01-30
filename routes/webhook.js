278bf633c109dc8b3618b4fc7729306bda57646d
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-30 12:00:52 -0500

    ##

--- routes/webhook.js
+++ routes/webhook.js
@@ -222,7 +222,7 @@ function handleApiAiResponse(sender, response) {
 
     if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1)) {
 
-        //console.log('Api.ai response messages' + JSON.stringify(messages))
+        console.log('Api.ai isDefined(messages) ' + JSON.stringify(messages))
         let timeoutInterval = 1100;
         let previousType;
         let cardTypes = [];
