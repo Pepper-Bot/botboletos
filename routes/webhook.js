4de8ca20957a4ddcb0a059a64503d870e28763a0
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-30 13:26:55 -0500

    #3

--- routes/webhook.js
+++ routes/webhook.js
@@ -871,19 +871,19 @@ function handleApiAiAction(sender, response, action, responseText, contexts, par
 
                             let qs = {
                                 entity_id: city_id, //location id 
-                                entity_type: "city", // location type (city,subzone,zone , landmark, metro,group) 
-                                q: "Cafe", //Search Keyword 
+                                entity_type: "city"//, // location type (city,subzone,zone , landmark, metro,group) 
+                               // q: "Cafe", //Search Keyword 
                                 // lat: "28.613939", //latitude 
                                 //lon: "77.209021", //longitude 
-                                count: "2", // number of maximum result to display 
-                                start: "1", //fetch results after offset 
+                                //count: "2", // number of maximum result to display 
+                                //start: "1", //fetch results after offset 
                                 //radius: "10000", //radius around (lat,lon); to define search area, defined in meters(M) 
                                 //cuisines: "3,7", //list of cuisine id's separated by comma 
                                 //establishment_type: "", //estblishment id obtained from establishments call 
                                 // collection_id: "29", //collection id obtained from collections call 
                                 //category: "9", //	category ids obtained from categories call 
-                                sort: " cost,rating,real_distance", //choose any one out of these available choices 
-                                order: "desc" //	used with 'sort' parameter to define ascending(asc )/ descending(desc) 
+                               //sort: " cost,rating,real_distance", //choose any one out of these available choices 
+                                //order: "desc" //	used with 'sort' parameter to define ascending(asc )/ descending(desc) 
 
                             }
 
