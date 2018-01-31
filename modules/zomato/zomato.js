c000e97bd3339a3aebd98fed5cc86fc7991d9bc9
leonardo jaimes <leo777jaimes@gmail.com>
2018-01-31 12:33:34 -0500

    ##

--- modules/zomato/zomato.js
+++ modules/zomato/zomato.js
@@ -209,7 +209,9 @@ var getCuisines = (city_id = 0, lat = 0, lon = 0, cuisine = '') => {
         let cuisinesR = JSON.parse(result);
         console.log('cuisines ' + JSON.stringify(cuisinesR))
         let cuisines = cuisinesR.cuisines
+        console.log('cuisines ' + JSON.stringify(cuisine))
         console.log('cuisines ' + JSON.stringify(cuisines))
+
         let cocina = query('cuisine.cuisine_name').is(cuisine).on(cuisines);
         //let cocina = query('cuisine.cuisine_name').startsWith(cuisine).or('cuisine.cuisine_name').endsWith(cuisine).on(cuisines);
 
