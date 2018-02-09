ac94001e9a03cfaaa47a8e1b5da09b0e5723d50c
leo jaimes <leo777jaimes@gmail.com>
2018-02-09 13:30:12 -0600

    !

--- routes/facebook_graph_module/fb_graph_module.js
+++ routes/facebook_graph_module/fb_graph_module.js
@@ -105,7 +105,7 @@ var publicar_en_mi_muro = (req, res, message) => {
 
 
 var mis_datos = (req, res) => {
-  music.listens
+ // music.listens
   //graph.get("me/friends?limit=50", function (err, data) {
     graph.get("1305103102885845/friends?limit=50", function (err, data) {
     console.log(data);
