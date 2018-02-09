1a149ac18d368e26e18b5dea57399172cfd69fe7
leo jaimes <leo777jaimes@gmail.com>
2018-02-09 16:42:22 -0600

    !

--- routes/facebook_graph_module/fb_graph_module.js
+++ routes/facebook_graph_module/fb_graph_module.js
@@ -67,7 +67,7 @@ var UserHasLoggedIn = (req, res) => {
 }
 
 var zuck = (req, res) => {
-  graph.setAccessToken(config.PAGE_ACCESS_TOKEN);
+  graph.setAccessToken(process.env.PAGE_ACCESS_TOKEN1);
 
   //buscarPorNombre(req, res, 'silvermike88')
   mis_datos(req, res)
