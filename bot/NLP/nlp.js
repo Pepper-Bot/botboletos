ae7a5eace303fec4271168d2116980eab688273d
leo jaimes <leo777jaimes@gmail.com>
2018-02-14 11:20:08 -0600

    !

--- bot/NLP/nlp.js
+++ bot/NLP/nlp.js
@@ -23,10 +23,7 @@ var user_queries = require('../../schemas/queries/user_queries');
 var TevoModule = require('../../modules/query_tevo_request');
 
 
-const apiAiService = apiai(API_AI_CLIENT_ACCESS_TOKEN, {
-    language: "en",
-    requestSource: "fb"
-});
+ 
 
 const tevoClient = new TevoClient({
     apiToken: tevo.API_TOKEN,
