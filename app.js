e6c67548a92ac7451ba83c5a1d26362bdaa2d61b
leo jaimes <leo777jaimes@gmail.com>
2018-02-16 12:21:29 -0600

    spotify longin

--- app.js
+++ app.js
@@ -379,9 +379,15 @@ app.get('spotify/logout', function(req, res){
 
 
 
-
-
-
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed. Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('spotify/login');
+}
 
 
 module.exports = app;
