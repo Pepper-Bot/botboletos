708e74c0f095237df3fa65a4f40ce4ee3d9b9821
leo jaimes <leo777jaimes@gmail.com>
2018-02-16 18:38:01 -0600

    !

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.use(function (req, res, next) {
 
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Origin", "https://botboletos-test.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", APLICATION_URL_DOMAIN);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
