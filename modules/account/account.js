8d199f73ca928beba546b6d774ee620362a8d5a0
leo.jaimes <leo777jaimes@gmail.com>
2018-03-19 17:17:01 -0600

    !

--- modules/account/account.js
+++ modules/account/account.js
@@ -2,15 +2,13 @@ const Messages = require("../../bot/generic_buttton");
 var APLICATION_URL_DOMAIN = require("../../config/config_vars")
   .APLICATION_URL_DOMAIN;
 
-  var APPLICATION_URL_EXTENSION = require("../../config/config_vars")
+var APPLICATION_URL_EXTENSION = require("../../config/config_vars")
   .APPLICATION_URL_EXTENSION;
 
-
-
 var startAccount = senderId => {
-    console.log('Entré a startAccount')
+  console.log("Entré a startAccount");
   var URLAplication = APLICATION_URL_DOMAIN + "redirect/?u=";
- var urlExtension = `${APPLICATION_URL_EXTENSION}profile?senderId=${senderId}` 
+  var urlExtension = `${APPLICATION_URL_EXTENSION}profile?senderId=${senderId}`;
 
   var boletos = [
     {
@@ -21,36 +19,32 @@ var startAccount = senderId => {
     }
   ];
 
-
   for (var i = 0, c = boletos.length; i < c; i++) {
+    console.log("i" + i);
     eventResults.push({
-        "title": boletos[i].titulo,
-        "image_url": boletos[i].imagen,
-        "subtitle": boletos[i].subtitulo,
-        //"item_url": boletos[i].url,
-        "default_action": {
-            "type": "web_url",
-            "url": boletos[i].url ,
-             "messenger_extensions": true,
-             "webview_height_ratio": "tall"
-            // "fallback_url": boletos[i].url
-        },
-        "buttons": [{
-                "type": "web_url",
-                "url": boletos[i].url,
-                "title": "Select your favorite artists"
-                //"payload": "TIBURON" + (i + 1)
-            }
-        ]
+      title: boletos[i].titulo,
+      image_url: boletos[i].imagen,
+      subtitle: boletos[i].subtitulo,
+      //"item_url": boletos[i].url,
+      default_action: {
+        type: "web_url",
+        url: boletos[i].url,
+        messenger_extensions: true,
+        webview_height_ratio: "tall"
+        // "fallback_url": boletos[i].url
+      },
+      buttons: [
+        {
+          type: "web_url",
+          url: boletos[i].url,
+          title: "Select your favorite artists"
+          //"payload": "TIBURON" + (i + 1)
+        }
+      ]
     });
 
-
-
-    console.log('events Results >>>>>>>>>>>>>>>' + eventResults[i].url);
-}
-
-
-
+    console.log("events Results >>>>>>>>>>>>>>>" + eventResults[i].url);
+  }
 
   Messages.genericTemplate(senderId, eventResults, "horizontal");
 };
