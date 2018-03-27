02356f3285c82a082a2f0e276d115c32af510d95
leo.jaimes <leo777jaimes@gmail.com>
2018-03-27 13:03:17 -0600

    !

--- modules/query_tevo_request.js
+++ modules/query_tevo_request.js
@@ -125,6 +125,7 @@ module.exports = function () {
                                             {
                                                 "type": "web_url",
                                                 "messenger_extensions": true,
+                                                "webview_height_ratio": "tall",
                                                 "url": baseURL + resultEvent[j].id + '&uid=' + senderId + '&venue_id=' + resultEvent[j].venue.id + '&performer_id=' + resultEvent[j].performances[0].performer.id + '&event_name=' + resultEvent[j].name,
                                                 "title": "Book"
                                             },
