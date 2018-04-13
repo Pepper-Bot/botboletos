03ab8d203fabca22710c3299343bd2e86593044c
leo.jaimes <leo777jaimes@gmail.com>
2018-04-13 18:29:14 -0500

    !

--- db/queries/user.notification.sheduled.queries.js
+++ db/queries/user.notification.sheduled.queries.js
@@ -347,7 +347,7 @@ var createUpdateUserNotificationSheduled = (fbId = "") => {
               userNotificationSheduled.nextNotificationDate =
                 userNotificationSheduled.nextNotificationDate.getTime() +
                 //1000 * 3600 * 24 * 3;
-                1000 * 10;
+                1000 * 3600 * 24 * 60;
               userNotificationSheduled.numberOfNextDays = 9999;
               console.log(
                 `userNotificationSheduled.nextNotificationDate  ${
