e47072efcfa5fe24aaa267458bb416ba359c58e5
leo.jaimes <leo777jaimes@gmail.com>
2018-05-08 11:14:39 -0500

    !

--- bot/me.send.js
+++ bot/me.send.js
@@ -1,6 +1,6 @@
 const FBMESSAGESPAGE = "https://graph.facebook.com/v2.6/me/messages";
 const PAGE_ACCESS_TOKEN = require("../config/config_vars").PAGE_ACCESS_TOKEN;
-const dashbot = require("dashbot")("CJl7GFGWbmStQyF8dYjR6WxIBPwrcjaIWq057IOO")
+const dashbot = require("dashbot")("n5G4hVmVw4MJ2cFAgrP3tSgfVnkPimXnN9b99zMv")
   .facebook; //new
 
 var request = require("request");
