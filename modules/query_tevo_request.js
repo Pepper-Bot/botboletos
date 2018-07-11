a4a1e57110b30fb2631e2fcee79a3668c5d8eaaf
leo.jaimes <leo777jaimes@gmail.com>
2018-07-11 18:39:33 -0500

    !

--- modules/query_tevo_request.js
+++ modules/query_tevo_request.js
@@ -379,10 +379,10 @@ var setImagesToEvents = (resultEvents, counter) => {
 
 
 
-          if (gButtons[z].id == 1104021) { ///final 
+          if (gButtons[z].id === 1104021) {
             gButtons[z].title = "FIFA World Cup Soccer Third Place Belgica Vs Inglaterra"
             gButtons[z].image_url = "https://ticketdelivery.herokuapp.com/images/fifa_worl_cup/belgica_inglaterra.jpg"
-          } else if (gButtons[z].id == 1104026) { //third place 
+          } else if (gButtons[z].id === 1104026) {
             gButtons[z].title = "FIFA World Cup Soccer Finals Francia Vs Croatia"
             gButtons[z].image_url = "https://ticketdelivery.herokuapp.com/images/fifa_worl_cup/francia_croatia.jpg"
           }
